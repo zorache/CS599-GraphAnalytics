990e6b47b9c5fdc04d31be27aa8d8125beb619e5
zorache <55007319+zorache@users.noreply.github.com>
2021-11-18 16:26:19 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -157,8 +157,7 @@ class AVLTree():
         self.preOrder(root.l)
         self.preOrder(root.r)
 
-        
-def TreeBuilder(file_name, header = True, skip = 0, txt = False):
+def TreeBuilder(file_name, header = True, skip = 0, txt = False,reverse=False):
     Tree = AVLTree()
     root = None
     with open(file_name, newline='') as file:
@@ -170,7 +169,10 @@ def TreeBuilder(file_name, header = True, skip = 0, txt = False):
                     continue
                 edge = (line.strip('\n')).strip('\r').split("\t")
                 print(edge)
-                root = Tree.insert(root, int(edge[0]),int(edge[1]))
+                if reverse:
+                    root = Tree.insert(root, int(edge[1]),int(edge[0]))
+                else:
+                    root = Tree.insert(root, int(edge[0]),int(edge[1]))
         else:
             reader = csv.reader(file, delimiter=' ')
             if header:
@@ -179,6 +181,28 @@ def TreeBuilder(file_name, header = True, skip = 0, txt = False):
             for edge in reader:
                 root = Tree.insert(root, int(edge[0]),int(edge[1]))
     return Tree, root
+
+# def TreeBuilder(file_name, header = True, skip = 0, txt = False):
+#     Tree = AVLTree()
+#     root = None
+#     with open(file_name, newline='') as file:
+#         if txt:
+#             lines = file.readlines()
+#             for line in lines:
+#                 if skip>0:
+#                     skip -=1
+#                     continue
+#                 edge = (line.strip('\n')).strip('\r').split("\t")
+#                 print(edge)
+#                 root = Tree.insert(root, int(edge[0]),int(edge[1]))
+#         else:
+#             reader = csv.reader(file, delimiter=' ')
+#             if header:
+#                 next(reader)
+#                 skip =skip -1
+#             for edge in reader:
+#                 root = Tree.insert(root, int(edge[0]),int(edge[1]))
+#     return Tree, root
         
         
 class TreijoinIterator(object):
