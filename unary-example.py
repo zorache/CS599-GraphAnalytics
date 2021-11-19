337d081375d3f0b20eb3998a64cc32bef2d5aa7a
zorache <55007319+zorache@users.noreply.github.com>
2021-11-19 04:43:50 -0500

    Update unary-example.py

--- unary-example.py
+++ unary-example.py
@@ -1,24 +1,20 @@
 from utils import *
 
+
+# Joining Two Tries for first column
 Tree = AVLTree()
 root = None
 root = Tree.insert(root, 1,1)
 root = Tree.insert(root, 2,1)
 root = Tree.insert(root, 3,3)
-# root = Tree.insert(root, 4)
-# root = Tree.insert(root, 5)
-# root = Tree.insert(root, 6)
-root = Tree.insert(root, 7, key_child=1)
+root = Tree.insert(root, 7, 1)
 
 Tree2 = AVLTree()
 root2 = None
 root2 = Tree2.insert(root2, 0,1)
 root2 = Tree2.insert(root2, 2,1)
 root2 = Tree2.insert(root2, 4,3)
-# root = Tree.insert(root, 4)
-# root = Tree.insert(root, 5)
-# root = Tree.insert(root, 6)
-root2 = Tree2.insert(root2, 7, key_child=1)
+root2 = Tree2.insert(root2, 7, 1)
 
 Iter = TreijoinIterator(Tree, root)
 Iter_2 = TreijoinIterator(Tree2, root2)
@@ -31,3 +27,4 @@ print(frog.Iter[1].key)
 while not frog.atEnd:
     frog.leapfrogNext()
 frog.key
+# Expected answer: [2,7]
